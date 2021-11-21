d85742a567fadbeb1c176b8f559e3bbb27e6e187
EllaMiri <ella.larsson0012@gmail.com>
2021-11-21 20:37:30 +0100

    created clock in js

--- main.js
+++ main.js
@@ -2,5 +2,32 @@ window.addEventListener('load', main)
 
 /** The start of the program */
 function main() {
-    console.log(main )
+    startClock();
+}
+
+/** Keeps the time updated on the page */
+function startClock() {
+setInterval(renderClock, 1000)
+}
+
+/** Updates the page with current time */
+function renderClock() {
+    const h2 = document.getElementById("time")
+    
+    const date = new Date()
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    h2.innerText = hours + ":"  + minutes + ":" + seconds;
 }
